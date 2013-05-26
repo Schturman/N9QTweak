9e656dbe28cf31e0045bad6b3d1cb0c0a62c1a06
Schturman <schturman@hotmail.com>
2013-05-26 22:32:23 +0300

    Update n9-qtweakmy.sh

--- n9-qtweakmy.sh
+++ n9-qtweakmy.sh
@@ -13,17 +13,14 @@ BLINK='\033[5m'
 GRAY='\033[1;37m'
 BGRED='\033[41m'
 
-# N9 QTweak v9.7.4
+# N9 QTweak v9.7.5
     echo " ---------------------------------------------"
     echo -e "|           ${CYAN}N9 QuickTweak changelog${NORMAL}           |"
     echo " ---------------------------------------------"
     echo -en ${YELLOW}
-    echo -e "* PDF instruction - updated."
-    echo -e "* RESTORE file - updated."
-    echo -e "* Tweak Z - updated."
-    echo -e "* Tweak F - updated option 11, added conf file that will check after reboot what you appied. Just reapply this tweak (for users that not use auto-background changer)."
-    echo -e "* Tweak FF - updated. To refresh button added reboot & shutdown. Also you can create reboot & shutdown buttons."
-    echo -e "* Tweak W - updated option 11, auto-background changer can change background of Quick-launch-bar too, just create images and put with your backgroud themes (see PDF instruction). Also added Quick-launch-bar mode (see PDF instruction)."
+    echo -e "* Tweak CC - updated option 3. Needed files moved from OBS to my dropbox and sripts updated. Also changed pasword for OpenVpn."
+    echo -e "* Tweak UU & Auto version checker - updated. Script moved from OBS to GitHub."
+    echo -e "* Tweak W - updated option 12, small update for restoring original icons."
     echo -en ${NORMAL}
     echo " ---------------------------------------------"
     echo ""
