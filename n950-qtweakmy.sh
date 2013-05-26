a909f233d76b7b8b770216daf6b4ea6053e28c5f
Schturman <schturman@hotmail.com>
2013-05-26 17:25:42 +0300

    Create n950-qtweakmy.sh

---
+++ n950-qtweakmy.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+RED='\033[1;31m'
+GREEN='\033[1;32m'
+PURPLE='\033[1;35m'
+CYAN='\033[1;36m'
+BLUE='\033[0;34m'
+INVERSE='\033[7m'
+NORMAL='\033[0m'
+YELLOW='\033[1;33m'
+UNDERLINE='\033[4m'
+BLINK='\033[5m'
+GRAY='\033[1;37m'
+BGRED='\033[41m'
+
+# N950 QTweak v9.7.4
+    echo " ---------------------------------------------"
+    echo -e "|          ${CYAN}N950 QuickTweak changelog${NORMAL}          |"
+    echo " ---------------------------------------------"
+    echo -en ${YELLOW}
+    echo -e "* PDF instruction - updated."
+    echo -e "* RESTORE file - updated."
+    echo -e "* Tweak Z - updated."
+    echo -e "* Tweak F - updated option 11, added conf file that will check after reboot what you appied. Just reapply this tweak (for users that not use auto-background changer)."
+    echo -e "* Tweak FF - updated. To refresh button added reboot & shutdown. Also you can create reboot & shutdown buttons."
+    echo -e "* Tweak W - updated option 11, auto-background changer can change background of Quick-launch-bar too, just create images and put with your backgroud themes (see PDF instruction). Also added Quick-launch-bar mode (see PDF instruction)."
+    echo -en ${NORMAL}
+    echo " ---------------------------------------------"
+    echo ""
