a90308f263109bcba10dd4a97f8c1222c6d9ff3e
Schturman <schturman@hotmail.com>
2013-06-08 17:06:39 +0300

    Update n9-qtweakmy.sh

--- n9-qtweakmy.sh
+++ n9-qtweakmy.sh
@@ -21,6 +21,51 @@ BGRED='\033[41m'
     echo -e "* Tweak CC - updated option 3. Needed files moved from OBS to my dropbox and sripts updated. Also changed pasword for OpenVpn."
     echo -e "* Tweak UU & Auto version checker - updated. Script moved from OBS to GitHub."
     echo -e "* Tweak W - updated option 12, small update for restoring original icons."
+    echo -e "${RED}* UPDATE (08.06.13)${YELLOW} - updated auto-background & full refresh scripts. Now auto-background changer will check if your phone busy (call/installation/GPS), and if yes, it will NOT change your background."
     echo -en ${NORMAL}
     echo " ---------------------------------------------"
+if [ ! -f /opt/N9QTweak/Autowall/auto-back1.sh ]; then
+:
+else
+line1=$(grep -n 'sleep' /opt/N9QTweak/Autowall/auto-back1.sh | sed 's/    //' | cut -d ':' -f1 | tail -1)
+line2=$(grep -n 'sleep' /opt/N9QTweak/Autowall/auto-back1.sh | sed 's/    //' | cut -d ':' -f2 | tail -1)
+if [ $line1 == "41" ]; then
+:
+else
+echo "
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$line2" >> /opt/N9QTweak/Autowall/auto-back1.sh
+fi
+fi
     echo ""
