b4a7200bb8c47c48d69ddfde551fd675bba77eb6
Schturman <schturman@hotmail.com>
2013-07-20 02:38:49 +0300

    Update n9-qtweakmy.sh

--- n9-qtweakmy.sh
+++ n9-qtweakmy.sh
@@ -13,61 +13,14 @@ BLINK='\033[5m'
 GRAY='\033[1;37m'
 BGRED='\033[41m'
 
-# N9 QTweak v9.7.8
+# N9 QTweak v9.8.0
     echo " ---------------------------------------------"
     echo -e "|           ${CYAN}N9 QuickTweak changelog${NORMAL}           |"
     echo " ---------------------------------------------"
     echo -en ${YELLOW}
     echo -e "* PDF instruction - updated."
-    echo -e "* Tweak B - updated. Option 5 moved to tweak Y."
-    echo -e "* Tweak Y - updated. Added option 8 from tweak B."
-    echo -e "* Tweak KK - updated option 11 from tweak KK. PowerPack moved to HH."
-    echo -e "* Tweak HH - updated. Tweak moved to KK. Now this tweak include PowerPack & Busybox-power."
-    echo -e "* Tweak EE - updated. Option 4 moved to HH. Origin fixer updated and now more safer..."
+    echo -e "* RESTORE file - updated."
+    echo -e "* Tweak - Almost all tweaks updated, Inception removed and instead this added Coderus aegis-install & opensudo. Check tweak EE."
     echo -en ${NORMAL}
     echo " ---------------------------------------------"
-if [ ! -f /opt/N9QTweak/Autowall/auto-back1.sh ]; then
-:
-else
-line1=$(grep -n 'sleep' /opt/N9QTweak/Autowall/auto-back1.sh | sed 's/    //' | cut -d ':' -f1 | tail -1)
-line2=$(grep -n 'sleep' /opt/N9QTweak/Autowall/auto-back1.sh | sed 's/    //' | cut -d ':' -f2 | tail -1)
-if [ $line1 == "41" ]; then
-:
-else
-echo "
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-$line2" >> /opt/N9QTweak/Autowall/auto-back1.sh
-fi
-fi
     echo ""
