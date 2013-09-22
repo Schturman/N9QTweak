7ce1add6064d19b0394ca787b2a7fb95fc2cf6ca
Schturman <schturman@hotmail.com>
2013-09-23 01:01:38 +0200

    Update n950-qtweakmy.sh

--- n950-qtweakmy.sh
+++ n950-qtweakmy.sh
@@ -13,14 +13,19 @@ BLINK='\033[5m'
 GRAY='\033[1;37m'
 BGRED='\033[41m'
 
-# N950 QTweak v9.8.5
+# N950 QTweak v9.8.6
     echo " ---------------------------------------------"
     echo -e "|          ${CYAN}N950 QuickTweak changelog${NORMAL}          |"
     echo " ---------------------------------------------"
     echo -en ${YELLOW}
     echo -e "* PDF instaruction - updated."
-    echo -e "* Tweak X - updated DEB packages creator."
-    echo -e "* Tweak T - updated option 3 (thanks to Coderus). New sys-ui and unrestricted setting."
+    echo -e "* RESTORE file - updated, added checking command about Open mode, Coderus aegis-install and opensudo."
+    echo -e "* Tweaks B, BB, CC, E, HH, II, KK, M, R, RR, T, X, Y, Z - added checking commands about Open mode, Coderus aegis-install, clean backup and opensudo where it needed."
+    echo -e "* Tweaks DD, F, R, T, W, Y - added new checking commands for color and font size (light & bold) changing tweaks to prevent user mistakes."
+    echo -e "* Tweak HH - Some commands updated. Also added Coderus Debinstaller & Coderus Power Menu QML (see PDF instruction)."
+    echo -e "* Tweak RR - Added option to install trusted apps (see PDF instruction)."
+    echo -e "* Tweak T - From T-3 removed option 0 (will be installed automatically if needed). Option 0 (zero) leaved as hidden option. Also added option to reostore all toggles in widgets applet if you edited it with GConfik."
+    echo -e "* Tweak X - Added option to install trusted apps (see PDF instruction). Also updated version of deb packager."
     echo -en ${NORMAL}
     echo " ---------------------------------------------"
     echo ""
